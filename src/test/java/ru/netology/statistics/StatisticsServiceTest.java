e505714374b9a83cf27c1d7aabc5184bd52169a2
Lera Lyakisheva <leralyakisheva@gmail.com>
2021-12-17 16:27:08 +0300

    changed StatisticsServiceTest

--- src/test/java/ru/netology/statistics/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistics/StatisticsServiceTest.java
@@ -9,8 +9,8 @@ class StatisticsServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long[] incomesInBillions = {12, 25, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 25;
 
         long actual = service.findMax(incomesInBillions);
 
